cf4bb17df5273d00effba2e434e18380ee2e1417
Ran Lottem <ran.lottem@philips.com>
2019-09-29 00:28:23 +0300

    updated summary report, deleted unnecessary files

--- models/cifar10_vgg_dual_selectivenet.py
+++ models/cifar10_vgg_dual_selectivenet.py
@@ -258,7 +258,7 @@ class cifar10vgg:
         # training parameters
         batch_size = 128
 
-        maxepoches = 300
+        maxepoches = 2
         learning_rate = 0.1
 
         lr_decay = 1e-6
--- selectivnet_utils.py
+++ selectivnet_utils.py
@@ -94,34 +94,3 @@ def my_dual_generator(func, x_train, y_train, batch_size):
         res = func(x_train, y_train, batch_size
                    ).next()
         yield [res[0], [res[1], res[1][:, :-1], res[0]]]
-
-
-def create_cats_vs_dogs_npz(cats_vs_dogs_path='datasets'):
-    labels = ['cat', 'dog']
-    label_to_y_dict = {l: i for i, l in enumerate(labels)}
-
-    def _load_from_dir(dir_name):
-        glob_path = os.path.join(cats_vs_dogs_path, dir_name, '*.*.jpg')
-        imgs_paths = glob(glob_path)
-        images = [resize_and_crop_image(p, 64) for p in imgs_paths]
-        x = np.stack(images)
-        y = [label_to_y_dict[os.path.split(p)[-1][:3]] for p in imgs_paths]
-        y = np.array(y)
-        return x, y
-
-    x_train, y_train = _load_from_dir('train')
-    x_test, y_test = _load_from_dir('test')
-
-    np.savez_compressed(os.path.join(cats_vs_dogs_path, 'cats_vs_dogs.npz'),
-                        x_train=x_train, y_train=y_train,
-                        x_test=x_test, y_test=y_test)
-
-
-def load_cats_vs_dogs(cats_vs_dogs_path='datasets/'):
-    npz_file = np.load(os.path.join(cats_vs_dogs_path, 'cats_vs_dogs.npz'))
-    x_train = npz_file['x_train']
-    y_train = npz_file['y_train']
-    x_test = npz_file['x_test']
-    y_test = npz_file['y_test']
-
-    return (x_train, y_train), (x_test, y_test)
